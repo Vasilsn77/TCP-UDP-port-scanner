646e93b4f9e2560d740fdf72df08eb472d78ccd0
Vasil S N <140039563+Vasilsn77@users.noreply.github.com>
2024-08-25 12:16:38 +0530

    Add files via upload

---
+++ FINAL.py
@@ -0,0 +1,172 @@
+import socket
+import threading
+import tkinter as tk
+from tkinter import ttk
+from queue import Queue
+from datetime import datetime
+import os
+import ssl
+
+class PortScannerGUI:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Port Scanner")
+        self.root.geometry("700x550")
+
+        # Styling for the entire window
+        self.root.style = ttk.Style()
+        self.root.style.configure("TButton", background="green", foreground="black")
+
+        #  target IP/Domain input
+        self.label_host = ttk.Label(root, text="Target IP/Domain:")
+        self.label_host.grid(row=0, column=0, padx=10, pady=10, sticky="w")
+
+        # Entry  target IP/Domain input
+        self.entry_host = ttk.Entry(root, width=30)
+        self.entry_host.grid(row=0, column=1, padx=10, pady=10)
+
+        #  server ports input
+        self.label_ports = ttk.Label(root, text="Server Port(s) (Example- 80,124 or leave empty to scan all ports):")
+        self.label_ports.grid(row=1, column=0, padx=10, pady=10, sticky="w")
+
+        # Entry  server ports input
+        self.entry_ports = ttk.Entry(root, width=30)
+        self.entry_ports.grid(row=1, column=1, padx=10, pady=10)
+
+        # Button to trigger port scanning
+        self.button_scan = ttk.Button(root, text="Scan", command=self.scan_ports, style="TButton")
+        self.button_scan.grid(row=0, column=2, padx=10, pady=10, rowspan=2, sticky="nsew")
+
+        #  displaying scan results
+        self.label_output = ttk.Label(root, text="Scan Results:")
+        self.label_output.grid(row=2, column=0, padx=10, pady=5, sticky="w")
+
+        # Text widget for displaying scan results
+        self.text_output = tk.Text(root, height=20, width=70, background='#808080', foreground='black')
+        self.text_output.grid(row=3, column=0, columnspan=3, padx=10, pady=10)
+
+    def clear(self):
+        # clear the terminal screen
+        if os.name == 'nt':
+            _ = os.system('cls')  # Windows
+        else:
+            _ = os.system('clear')  # Unix/Linux
+
+    def scan_ports(self):
+        start_time = datetime.now()  # Record the start time
+        # initiate the port scanning process
+        host = self.entry_host.get()
+        ports_input = self.entry_ports.get().strip()
+
+        if ports_input == "":
+            start_port, end_port = 1, 65535  # if port range is not specified, scan all ports
+        elif ":" in ports_input:
+            # If port range is specified as start:end
+            try:
+                start_port, end_port = map(int, ports_input.split(':'))
+            except:
+                self.text_output.insert("end", "Invalid port range. Please enter ports in the format 'start:end'.\n")
+                return
+        else:
+            # If a single port is specified
+            try:
+                start_port = end_port = int(ports_input)
+            except:
+                self.text_output.insert("end", "Invalid port number. Please enter a valid port number.\n")
+                return
+
+        if start_port > end_port:
+            # Check if the start port is greater than the end port
+            self.text_output.insert("end", "Invalid port range. Start port cannot be greater than end port.\n")
+            return
+
+        self.clear()  # Clear the console/terminal screen
+        self.text_output.delete(1.0, "end")  # Clear previous scan results
+        self.text_output.insert("end", "Port Scanner\n")
+        self.text_output.insert("end", f"Target IP: {host}\n")
+
+        # Running the scanning process in a separate thread
+        threading.Thread(target=self.run_scanner, args=(host, start_port, end_port), daemon=True).start()
+
+    def run_scanner(self, host, start_port, end_port):
+        # Function to perform port scanning
+
+        threads = 2048  # Number of threads for scanning
+
+        def scan(port, protocol):
+            # Function to scan a specific port
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM if protocol == 'tcp' else socket.SOCK_DGRAM)
+            s.settimeout(5)  # Timeout for socket connection
+
+            if protocol == 'tcp':
+                try:
+                    if port == 443:  # If port is 443, use SSL/TLS
+                        s = ssl.wrap_socket(s, ssl_version=ssl.PROTOCOL_TLS)
+                    s.connect((host, port))  # Check TCP connection
+                    s.close()
+                    return True
+                except socket.error:
+                    return False
+            else:
+                try:
+                    s.sendto(b'', (host, port))  # Check UDP connection
+                    s.close()
+                    return True
+                except socket.error:
+                    return False
+
+        queue = Queue()  # Queue to store ports for scanning
+
+        def get_ports():
+            # Function to populate the queue with ports to scan
+            for port in range(start_port, end_port + 1):
+                queue.put(port)
+
+        open_ports_tcp = []  # List to store open TCP ports
+        open_ports_udp = []  # List to store open UDP ports
+
+        def worker():
+            # Worker function for each thread to scan ports
+            while not queue.empty():
+                port = queue.get()
+                if scan(port, 'tcp'):
+                    open_ports_tcp.append(port)
+                    service_name = socket.getservbyport(port, 'tcp')  # Get service name corresponding to the port
+                    self.text_output.insert("end", f"TCP Port {port} is open! Service: {service_name}\n")
+                if scan(port, 'udp'):
+                    open_ports_udp.append(port)
+                    service_name = socket.getservbyport(port, 'udp')  # Get service name corresponding to the port
+                    self.text_output.insert("end", f"UDP Port {port} is open! Service: {service_name}\n")
+
+        get_ports()  # Populate the queue with ports to scan
+
+        thread_list = []
+        for _ in range(threads):
+            # Create and start threads for port scanning
+            thread = threading.Thread(target=worker)
+            thread_list.append(thread)
+
+        for thread in thread_list:
+            thread.start()
+
+        for thread in thread_list:
+            thread.join()  # Wait for all threads to complete
+
+        # Check if port 443 is open
+        if 443 in open_ports_tcp :
+            self.text_output.insert("end", "TCP Port 443 is open!\nThe domain is secure and is using SSL.\n")
+        else:
+            self.text_output.insert("end", "TCP Port 443 is not open.\nThe domain may not be using SSL.\n")
+
+        self.text_output.insert("end", f"\nScanning complete at: {datetime.now()}\n")
+
+        self.text_output.insert("end", f"\n\nOPEN TCP PORTS ARE : {len(open_ports_tcp)}\n\n")
+        self.text_output.insert("end", f"\n\nOPEN UDP PORTS ARE : {len(open_ports_udp)}\n\n")
+
+def main():
+    root = tk.Tk()
+    port_scanner_gui = PortScannerGUI(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
